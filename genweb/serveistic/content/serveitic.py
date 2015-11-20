0b68ea6d6335b76bafe8fc965f432c7712a65114
hanirok <corina.riba@upcnet.es>
2015-11-20 14:04:31 +0100

    Not check email

--- genweb/serveistic/content/serveitic.py
+++ genweb/serveistic/content/serveitic.py
@@ -12,47 +12,17 @@ from genweb.serveistic import _
 from zope.interface import implements
 from plone.dexterity.content import Item
 
-from plone.app.users.userdataschema import checkEmailAddress
 from zope.app.container.interfaces import IObjectAddedEvent
-
-
-
-
-from AccessControl import Unauthorized
-from AccessControl import getSecurityManager
-
-from z3c.form import button
 
 from genweb.serveistic.interfaces import IGenwebServeisticLayer
 
-from zope.component import getMultiAdapter
-from zope.component import queryUtility
-from zope.component.hooks import getSite
-from zope.event import notify
 from zope.interface import Interface
 from zope.interface import alsoProvides
-from zope.lifecycleevent import ObjectModifiedEvent
-from zope.lifecycleevent.interfaces import IObjectModifiedEvent
-from zope.lifecycleevent.interfaces import IObjectRemovedEvent
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary
-from zope.security import checkPermission
 
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces import IPloneSiteRoot
+
 from Products.CMFPlone.interfaces.constrains import ISelectableConstrainTypes
 from plone.dexterity.utils import createContentInContainer
 
-from plone.indexer import indexer
-from plone.memoize.view import memoize_contextless
-from plone.portlets.constants import CONTEXT_CATEGORY
-from plone.portlets.interfaces import ILocalPortletAssignmentManager
-from plone.portlets.interfaces import IPortletManager
-from plone.registry.interfaces import IRegistry
-
-
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.portlets.portlets.navigation import Assignment as NavPortletAssignment
 from genweb.theme.browser.interfaces import IHomePageView
 from genweb.theme.browser.views import HomePageBase
 
@@ -81,8 +51,7 @@ class IServeiTIC(form.Schema):
     responsableMail = schema.TextLine(
         title=_(u'From mail'),
         description=_(u'Enter the from used in the mail form'),
-        required=True,
-        constraint=checkEmailAddress
+        required=False,
     )
 
     image = NamedBlobImage(
