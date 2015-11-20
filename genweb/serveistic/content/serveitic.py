930f45eed085a8ad145f37412f361b978ea24dd2
hanirok <corina.riba@upcnet.es>
2015-11-20 14:16:04 +0100

    Coma olvidada

--- genweb/serveistic/content/serveitic.py
+++ genweb/serveistic/content/serveitic.py
@@ -12,17 +12,47 @@ from genweb.serveistic import _
 from zope.interface import implements
 from plone.dexterity.content import Item
 
+from plone.app.users.userdataschema import checkEmailAddress
 from zope.app.container.interfaces import IObjectAddedEvent
+
+
+
+
+from AccessControl import Unauthorized
+from AccessControl import getSecurityManager
+
+from z3c.form import button
 
 from genweb.serveistic.interfaces import IGenwebServeisticLayer
 
+from zope.component import getMultiAdapter
+from zope.component import queryUtility
+from zope.component.hooks import getSite
+from zope.event import notify
 from zope.interface import Interface
 from zope.interface import alsoProvides
+from zope.lifecycleevent import ObjectModifiedEvent
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
+from zope.lifecycleevent.interfaces import IObjectRemovedEvent
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.security import checkPermission
 
-
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.CMFPlone.interfaces.constrains import ISelectableConstrainTypes
 from plone.dexterity.utils import createContentInContainer
 
+from plone.indexer import indexer
+from plone.memoize.view import memoize_contextless
+from plone.portlets.constants import CONTEXT_CATEGORY
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from plone.portlets.interfaces import IPortletManager
+from plone.registry.interfaces import IRegistry
+
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.portlets.portlets.navigation import Assignment as NavPortletAssignment
 from genweb.theme.browser.interfaces import IHomePageView
 from genweb.theme.browser.views import HomePageBase
 
@@ -52,6 +82,7 @@ class IServeiTIC(form.Schema):
         title=_(u'From mail'),
         description=_(u'Enter the from used in the mail form'),
         required=False,
+        constraint=checkEmailAddress
     )
 
     image = NamedBlobImage(
