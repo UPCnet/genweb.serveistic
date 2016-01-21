c4c2db559ec56278f303e8da07b148527bc0ec7d
Paco Gregori <francisco.gregori@upcnet.es>
2016-01-21 13:16:35 +0100

    add requisits to faceted search

--- genweb/serveistic/utilities.py
+++ genweb/serveistic/utilities.py
@@ -10,6 +10,13 @@ from plone.registry.interfaces import IRegistry
 from Acquisition import aq_inner, aq_chain
 from genweb.serveistic.content.serveitic import IServeiTIC
 
+import operator
+from eea.faceted.vocabularies.utils import compare
+from eea.faceted.vocabularies.utils import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+from Products.CMFCore.utils import getToolByName
+
 
 class IKeywordsCategorizationUtility(Interface):
     """ Utility to manage a registry of the items used in the newsletters
@@ -79,3 +86,86 @@ def get_servei(self):
         if IServeiTIC.providedBy(obj):
             return obj
     return None
+
+
+# --------------------
+# Faceted Vocabularys
+# --------------------
+class UbicacioVocabulary(object):
+    """ Return portal types as vocabulary
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        """ See IVocabularyFactory interface
+        """
+
+        facetes = serveistic_config().facetes_table
+        if facetes:
+            facetes_sorted = sorted(facetes, key=lambda x: x['faceta'])
+        res = []
+        for fac in facetes_sorted:
+            if fac['faceta'].encode('utf8') == 'Ubicació':
+                res.append(fac['valor'].encode('utf8'))
+        items = [SimpleTerm(value, value, value) for value in res]
+        return SimpleVocabulary(items)
+
+
+class PrestadorVocabulary(object):
+    """ Return portal types as vocabulary
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        """ See IVocabularyFactory interface
+        """
+
+        facetes = serveistic_config().facetes_table
+        if facetes:
+            facetes_sorted = sorted(facetes, key=lambda x: x['faceta'])
+        res = []
+        for fac in facetes_sorted:
+            if fac['faceta'].encode('utf8') == 'Prestador':
+                res.append(fac['valor'].encode('utf8'))
+        items = [SimpleTerm(value, value, value) for value in res]
+        return SimpleVocabulary(items)
+
+
+class TipologiaVocabulary(object):
+    """ Return portal types as vocabulary
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        """ See IVocabularyFactory interface
+        """
+
+        facetes = serveistic_config().facetes_table
+        if facetes:
+            facetes_sorted = sorted(facetes, key=lambda x: x['faceta'])
+        res = []
+        for fac in facetes_sorted:
+            if fac['faceta'].encode('utf8') == 'Tipologia':
+                res.append(fac['valor'].encode('utf8'))
+        items = [SimpleTerm(value, value, value) for value in res]
+        return SimpleVocabulary(items)
+
+
+class AmbitVocabulary(object):
+    """ Return portal types as vocabulary
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        """ See IVocabularyFactory interface
+        """
+
+        facetes = serveistic_config().facetes_table
+        if facetes:
+            facetes_sorted = sorted(facetes, key=lambda x: x['faceta'])
+        res = []
+        for fac in facetes_sorted:
+            if fac['faceta'].encode('utf8') == 'Àmbit':
+                res.append(fac['valor'].encode('utf8'))
+        items = [SimpleTerm(value, value, value) for value in res]
+        return SimpleVocabulary(items)
