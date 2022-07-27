5be74bdc9787baa745b597e93078e079e8233f06
Iago López <ilopezsmx2011@gmail.com>
2022-07-27 16:12:14 +0200

    Fix migracio facetes

--- genweb/serveistic/browser/migracio_facetes.py
+++ genweb/serveistic/browser/migracio_facetes.py
@@ -13,21 +13,21 @@ class MigracioFacetes(BrowserView):
         serveistic = catalog(portal_type='serveitic')
         for serveitic in serveistic:
             servei = serveitic.getObject()
-            servei.ca_faceta_1 = servei.prestador
-            servei.es_faceta_1 = servei.prestador
-            servei.en_faceta_1 = servei.prestador
+            servei.ca_faceta_1 = servei.tipologia
+            servei.es_faceta_1 = servei.tipologia
+            servei.en_faceta_1 = servei.tipologia
 
-            servei.ca_faceta_2 = servei.ubicacio
-            servei.es_faceta_2 = servei.ubicacio
-            servei.en_faceta_2 = servei.ubicacio
+            servei.ca_faceta_2 = servei.ambit
+            servei.es_faceta_2 = servei.ambit
+            servei.en_faceta_2 = servei.ambit
 
-            servei.ca_faceta_3 = servei.tipologia
-            servei.es_faceta_3 = servei.tipologia
-            servei.en_faceta_3 = servei.tipologia
+            servei.ca_faceta_3 = servei.ubicacio
+            servei.es_faceta_3 = servei.ubicacio
+            servei.en_faceta_3 = servei.ubicacio
 
-            servei.ca_faceta_4 = servei.ambit
-            servei.es_faceta_4 = servei.ambit
-            servei.en_faceta_4 = servei.ambit
+            servei.ca_faceta_4 = servei.prestador
+            servei.es_faceta_4 = servei.prestador
+            servei.en_faceta_4 = servei.prestador
 
             servei.reindexObject()
 
